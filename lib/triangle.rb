152a40aa419e7731e892afcb09a795930b0d5c01
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-27 23:54:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,8 +19,11 @@ class Triangle
 end
 
 def confirm_triangle
+  triangle = [(a + b > c), (a + c > b), (b + c > a)]
+  [a, b, c].each {|s| triangle << false if s <= 0}
+  raise TriangleError if triangle.include?(false)
+end
 end
 
 class TriangleError < StandardError
 end
-  
